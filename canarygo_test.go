df88acd7ee84e6c81b2b17b6d7e535dc5ab13753
andreburto <tuglyraisin@gmail.com>
2024-02-24 00:11:13 -0600

    Add tests.

---
+++ canarygo_test.go
@@ -0,0 +1,32 @@
+package canarygo
+
+import (
+	"io"
+	"os"
+	"strings"
+	"testing"
+)
+
+func TestCanary(t *testing.T) {
+	expected := "Black Canary, GO!"
+
+	// Capture the stdout from Canary().
+	originalStdout := os.Stdout
+  readIn, writeOut, _ := os.Pipe()
+  os.Stdout = writeOut
+
+	// Call the function and capture the output.
+	Canary()
+
+	// Revert to the original stdout.
+	writeOut.Close()
+	stdOutput, _ := io.ReadAll(readIn)
+	os.Stdout = originalStdout
+
+	// Convert the bytes to a string.
+	actual := string(stdOutput[:])
+
+	if strings.Contains(actual, expected) != true {
+		t.Error("Canary function did not contain extected output.")
+	}
+}
